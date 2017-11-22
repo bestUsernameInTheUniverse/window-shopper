1a26c35877dc455bb1233732869bcf98a428dbad
yurik <yuri.kromsky@gmail.com>
2017-11-21 16:40:12 -0800

    Initial commit.

---
+++ window-shopper/window-shopper/Controler/MainVC.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  window-shopper
+//
+//  Created by Yuri Kromsky on 11/21/17.
+//  Copyright © 2017 Yuri Kromsky. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
